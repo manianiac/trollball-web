cbf8958a62bf744f02b27d23588c9de58ab5e289
manianiac <williambarronnexus@gmail.com>
2025-11-14 04:16:16 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+// next.config.js
+
+// Replace 'my-repo-name' with the name of your GitHub repository
+const repoName = 'trollball-web'; 
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // Required for static export
+  output: 'export',
 
-module.exports = nextConfig
+  // Set the base path to your repo name
+  basePath: isProd ? `/${repoName}` : '',
+
+  // Set the asset prefix to your repo name
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
+  // RECOMMENDED: Disable image optimization for static exports
+  // (This avoids issues with next/image)
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
